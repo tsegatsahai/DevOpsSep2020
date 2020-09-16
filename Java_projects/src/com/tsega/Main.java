165a9d88b7662b06bb3c3c05b245d789c5d14c3d
Tsega Tsahai <tsegatsahai@Tsegas-MacBook-Air.local>
2020-09-16 14:46:55 -0500

    created the folder for the java programs

---
+++ Java_projects/src/com/tsega/Main.java
@@ -0,0 +1,8 @@
+package com.tsega;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("heyo");
+    }
+}
---
+++ Java_projects/src/com/tsega/Vowels.java
@@ -0,0 +1,10 @@
+package com.tsega;
+
+public class Vowels {
+    
+}
+
+//    Create Vowels class in JAVA and Vowels branch in GIT then do the following:
+//        - Count all the vowels in a sentence and display the count
+//        - Push Vowels class to Vowels branch in git
+//          - Merge Vowels branch changes into the repository
